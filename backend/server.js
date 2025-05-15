3a24212ff1c8f95f2358c3ada9969e881aaffe7e
Tushar Patil <143060635+Tushar7102@users.noreply.github.com>
2025-05-15 18:49:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000' , 'https://realestate-chatboat.vercel.app/'],
+  origin: ['https://realestate-chatboat.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
